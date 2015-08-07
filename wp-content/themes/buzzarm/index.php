3c3848352a60133e3cd810dfc5bc4dcfaa0dddc9
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-08-07 14:03:17 +0300

    fix

--- wp-content/themes/buzzarm/index.php
+++ wp-content/themes/buzzarm/index.php
@@ -1361,7 +1361,7 @@
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-<script src="js/jquery.rotator.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/buzzarm/jquery.rotator.min.js"></script>
 <script>
     $(document).ready(function() {
 
