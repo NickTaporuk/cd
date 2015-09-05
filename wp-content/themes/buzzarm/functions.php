0bedbc62155a86e9120f9e3a73485cc62dcc425a
NickTaporuk <nictaporuk@yandex.ru>
2015-09-05 17:32:59 +0300

    init admin console angular frontend

--- wp-content/themes/buzzarm/functions.php
+++ wp-content/themes/buzzarm/functions.php
@@ -79,110 +79,52 @@ add_menu_page( __(' Buzzarm Panel' ), __(' Buzzarm Panel' ), 'manage_options', '
 
 }
 
-/*---------------------------------------------------
-
-add actions
-
-----------------------------------------------------*/
 add_action( 'admin_init', 'theme_settings_init' );
 add_action( 'admin_menu', 'add_settings_page' );
-/*---------------------------------------------------
 
-Theme Panel Output
 
-----------------------------------------------------*/
-/* ----------------------------------------------------------
-Declare vars
-------------------------------------------------------------- */
-$themename = "Theme Name";
-$shortname = "shortname";
-$categories = get_categories('hide_empty=0&orderby=name');
-$all_cats = array();
-foreach ($categories as $category_item ) {
-	$all_cats[$category_item->cat_ID] = $category_item->cat_name;
+
+/*
+ * get all tables name in selected db
+ * **/
+function getNameTables() {
+	global $wpdb;
+	$getAllNameTAbles = 'SHOW TABLES;';
+	$results = $wpdb->get_results( $getAllNameTAbles );
+
+
+	if(!empty($results)){
+		$res = [];
+		foreach($results as $val){
+			foreach($val as $k=>$v) {
+				$res[] = $v;
+			}
+		}
+		echo json_encode(["status"=>1,'response' => $res]);exit;
+	} else {
+		echo json_encode(["status"=>1,'response' => false]);exit;
+	}
+	exit;
 }
-array_unshift($all_cats, "Select a category");
+add_action('wp_ajax_getNameTables', 'getNameTables');
+add_action('wp_ajax_nopriv_getNameTables', 'getNameTables');
 
-/* ---------------------------------------------------------
-Declare options
------------------------------------------------------------ */
-$theme_options = array (
-	array( "name" => $themename." Options",
-		"type" => "title"),
-	/* ---------------------------------------------------------
-      General section
-      ----------------------------------------------------------- */
-	array( "name" => "General",
-		"type" => "section"),
-	array( "type" => "open"),
+/*
+ * get name selected db
+ * **/
+function getNameDatabase() {
+		echo json_encode(['response' => (DB_NAME)?DB_NAME:false]);exit;
 
-	array( "name" => "Logo URL",
-		"desc" => "Enter the link to your logo image",
-		"id" => $shortname."_logo",
-		"type" => "text",
-		"std" => ""),
+}
+add_action('wp_ajax_getNameDatabase', 'getNameDatabase');
+add_action('wp_ajax_nopriv_getNameDatabase', 'getNameDatabase');
 
-	array( "name" => "Custom Favicon",
-		"desc" => "A favicon is a 16x16 pixel icon that represents your site; paste the URL to a .ico image that you want to use as the image",
-		"id" => $shortname."_favicon",
-		"type" => "text",
-		"std" => get_bloginfo('url') ."/images/favicon.ico"),
-
-	array( "type" => "close"),
-	/* ---------------------------------------------------------
-      Home section
-
-      ----------------------------------------------------------- */
-
-	array( "name" => "Homepage",
-
-		"type" => "section"),
-	array( "type" => "open"),
-	array( "name" => "Homepage Featured",
-		"desc" => "Choose a category from which featured posts are.",
-		"id" => $shortname."_feat_cat",
-		"type" => "select",
-		"options" => $all_cats,
-		"std" => "Select a category"),
-
-	array( "type" => "close"),
-
-/* ---------------------------------------------------------
-      Footer section
------------------------------------------------------------ */
-	array( "name" => "Footer",
-		"type" => "section"),
-	array( "type" => "open"),
-
-	array( "name" => "Footer Credit",
-		"desc" => "You can customize footer credit on footer area here.",
-		"id" => $shortname."_footer_text",
-		"type" => "text",
-		"std" => ""),
-
-	array( "type" => "close")
-
-);
 
 
 function theme_settings_page() {
-	global $themename,$theme_options;
-  $i=0;
-  $message='';
-  if ( 'save' == $_REQUEST['action'] ) {
-    foreach ($theme_options as $value) {
-      update_option( $value['id'], $_REQUEST[ $value['id'] ] ); }
-    foreach ($theme_options as $value) {
-      if( isset( $_REQUEST[ $value['id'] ] ) ) { update_option( $value['id'], $_REQUEST[ $value['id'] ]  ); } else { delete_option( $value['id'] ); } }
-    $message='saved';
-  }
-  else if( 'reset' == $_REQUEST['action'] ) {
+	global $themename,$theme_options,$wpdb;
 
-    foreach ($theme_options as $value) {
-      delete_option( $value['id'] ); }
-    $message='reset';
-  }
-
+//	var_dump(DB_NAME);
   ?>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/js/bower_components/bootstrap/dist/css/bootstrap.css"/>
 	<script src="<?php echo get_template_directory_uri()?>/js/bower_components/angular/angular.js"></script>
@@ -190,19 +132,10 @@ function theme_settings_page() {
 
 	<div class="wrap options_wrap" ng-app="App">
     <div id="icon-options-general"></div>
-    <h2><?php _e( ' Buzzarm Options' ) //your admin panel title ?></h2>
-    <?php
-    if ( $message=='saved' ) echo '<div class="updated settings-error" id="setting-error-settings_updated">
-    <p>'.$themename.' settings saved.</strong></p></div>';
-    if ( $message=='reset' ) echo '<div class="updated settings-error" id="setting-error-settings_updated">
-    <p>'.$themename.' settings reset.</strong></p></div>';
-    ?>
-    <ul>
-		<li>View Documentation |</li>
-		<li>Visit Support |</li>
-		<li>Theme version 1.0 </li>
-	</ul>
+    <h2><?php _e( ' Buzzarm Options' )?></h2>
+
 	<div class="content_options" ng-controller="adminCtrl">
+	<h3>Selected database name: {{ nameDb }}</h3>
 	<table id="example1" class="table table-bordered table-striped">
 		<thead>
 		<tr>
@@ -257,94 +190,5 @@ function theme_settings_page() {
 		</tr>
 		</tfoot>
 	</table>
-	<form method="post">
-      <?php foreach ($theme_options as $value) {
-
-        switch ( $value['type'] ) {
-
-			case "open": ?>
-				<?php break;
-
-          case "close": ?>
-			  </div>
-			  </div><br />
-			  <?php break;
-
-          case "title": ?>
-			  <div class="message">
-				  <p>To easily use the <?php echo $themename;?> theme options, you can use the options below.</p>
-			  </div>
-			  <?php break;
-
-          case 'text': ?>
-			  <div class="option_input option_text">
-				  <label for="<?php echo $value['id']; ?>">
-					  <?php echo $value['name']; ?></label>
-				  <input id="" type="<?php echo $value['type']; ?>" name="<?php echo $value['id']; ?>" value="<?php if ( get_settings( $value['id'] ) != "") { echo stripslashes(get_settings( $value['id'])  ); } else { echo $value['std']; } ?>" />
-				  <small><?php echo $value['desc']; ?></small>
-				  <div class="clearfix"></div>
-			  </div>
-			  <?php break;
-
-          case 'textarea': ?>
-			  <div class="option_input option_textarea">
-				  <label for="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></label>
-				  <textarea name="<?php echo $value['id']; ?>" rows="" cols=""><?php if ( get_settings( $value['id'] ) != "") { echo stripslashes(get_settings( $value['id']) ); } else { echo $value['std']; } ?></textarea>
-				  <small><?php echo $value['desc']; ?></small>
-				  <div class="clearfix"></div>
-			  </div>
-			  <?php break;
-
-          case 'select': ?>
-			  <div class="option_input option_select">
-				  <label for="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></label>
-				  <select name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>">
-					  <?php foreach ($value['options'] as $option) { ?>
-						  <option <?php if (get_settings( $value['id'] ) == $option) { echo 'selected="selected"'; } ?>><?php echo $option; ?></option>
-					  <?php } ?>
-				  </select>
-				  <small><?php echo $value['desc']; ?></small>
-				  <div class="clearfix"></div>
-			  </div>
-			  <?php break;
-
-          case "checkbox": ?>
-			  <div class="option_input option_checkbox">
-				  <label for="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></label>
-				  <?php if(get_option($value['id'])){ $checked = "checked=\"checked\""; }else{ $checked = "";} ?>
-				  <input id="<?php echo $value['id']; ?>" type="checkbox" name="<?php echo $value['id']; ?>" value="true" <?php echo $checked; ?> />
-				  <small><?php echo $value['desc']; ?></small>
-				  <div class="clearfix"></div>
-			  </div>
-			  <?php break;
-
-          case "section":
-          $i++; ?>
-          <div class="input_section">
-          <div class="input_title">
-
-			  <h3><img src="<?php echo get_template_directory_uri();?>/images/options.png" alt="">&nbsp;<?php echo $value['name']; ?></h3>
-			  <span class="submit"><input name="save<?php echo $i; ?>" type="submit" class="button-primary" value="Save changes" /></span>
-			  <div class="clearfix"></div>
-		  </div>
-          <div class="all_options">
-			  <?php break;
-
-        }
-      }?>
-      <input type="hidden" name="action" value="save" />
-      </form>
-      <form method="post">
-		  <p class="submit">
-			  <input name="reset" type="submit" value="Reset" />
-			  <input type="hidden" name="action" value="reset" />
-		  </p>
-	  </form>
-	</div>
-    <div class="footer-credit">
-		<p>This theme was made by <a title="buzzarm" href="buzzarm.com" target="_blank" >Buzzarm</a>.</p>
-	</div>
-	</div>
-
 <?php
 }
--- wp-content/themes/buzzarm/js/admin/app.js
+++ wp-content/themes/buzzarm/js/admin/app.js
@@ -1,18 +1,43 @@
  var app = angular.module("App",[]);
 
-
- app.controller('adminCtrl',['$scope',function($scope){
-     $scope.name = "Nick Taporuk";
-
-     $scope.user = {
-         name:'Nick',
-         surname:'Taporuk',
-         company:'ITFTC'
-     };
-
-     $scope.jonh = {
-         name:'Jonh',
-         surname:'Dauhn',
-         company:'Genesis'
-     };
+ app.factory('tablesData',['$http',function($http){
+     var link = "/wp-admin/admin-ajax.php",
+         actionsNameDb = "getNameDatabase",
+         actionsNamesTables = "getNameTables",
+         actionsWp = {method:'GET', url:link,params:{"action":''}};
+     return {
+         getNameDB:function(callback){
+             //actionsWp.params.action = actionsNameDb;
+             actionsWp = {method:'GET', url:link,params:{"action":"getNameDatabase"}};
+             return $http(actionsWp).success(callback);
+         },
+         getNamesTables:function(callback){
+             actionsWp = {method:'GET', url:link,params:{"action":"getNameTables"}};
+             //actionsWp.params.action = actionsNamesTables;
+             return $http(actionsWp).success(callback);
+         }
+     }
  }]);
+
+ app.controller('adminCtrl',['$scope','$http','tablesData',function($scope,$http,tablesData){
+     $scope.nameTables  = '';
+     $scope.nameDb = '';
+
+
+     tablesData.getNameDB(function(response) {
+         $scope.nameDb = response.response;
+         //$scope.nameDb = response.data.response;
+         console.log('response::',$scope.nameDb);
+     });
+
+     tablesData.getNamesTables(function(response) {
+         $scope.nameTables = response.response;
+         //$scope.nameDb = response.data.response;
+         console.log('response nameTables::',$scope.nameTables);
+     });
+
+     console.log('$scope.nameDb::',$scope.nameDb);
+     //console.log('tablesData.getNameDB()::',tablesData.getNameDB());
+
+ }]);
+
