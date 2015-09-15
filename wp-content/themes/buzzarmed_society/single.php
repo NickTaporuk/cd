b9233cb2ded02150fec1a5c7f25617da2f615a42
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-09-15 11:45:52 +0300

    add views and comments to content-single.php

--- wp-content/themes/buzzarmed_society/single.php
+++ wp-content/themes/buzzarmed_society/single.php
@@ -14,11 +14,14 @@ get_header(); ?>
 <div class="content-left-wrap col-md-9">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php while ( have_posts() ) : the_post(); 
-				
+
+
+		<?php while ( have_posts() ) : the_post();
+				?>
+				<?php
 				 get_template_part( 'content', 'single' );
 				 
-				 zerif_post_nav(); 
+//				 zerif_post_nav();
 			 
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || '0' != get_comments_number() ) :
